0f8848adcda2981be641ed0ec0ce5d7041cdf21e
SeungHeeKo5 <SeungHeeKo5@users.noreply.github.com>
2026-04-11 03:52:47 +0900

    Reset project: Nuke all files, keep only title

--- my-profile/README.md
+++ my-profile/README.md
@@ -1,3 +1,1 @@
-# My Link
-
-Hello World!!
+# my-profile
--- my-profile/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="min-h-screen flex items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">SEUNGHEE KO</h1>
-    </main>
-  );
-}
